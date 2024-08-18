1642ab97b6961299f96eee52325b2a4986200b9e
Александр Буйный <Buapoliv@gmail.com>
2024-08-18 13:09:32 +0300

    push to review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-   'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
