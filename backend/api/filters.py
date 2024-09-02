fc9024f85cf2807875d88dfb26d511f0235cab12
Александр Буйный <Buapoliv@gmail.com>
2024-09-03 02:39:43 +0300

    fix tags filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,8 +1,9 @@
-# import django_filters
-from django_filters.rest_framework import BooleanFilter, FilterSet, AllValuesMultipleFilter, ModelMultipleChoiceFilter
+from django_filters.rest_framework import (AllValuesMultipleFilter,
+                                           BooleanFilter, FilterSet,
+                                           ModelMultipleChoiceFilter)
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
+from recipes.models import Recipe
 from users.models import User
 
 
