5653fca90a6d73c0383d29038f1fd97b1f98c724
Александр Буйный <Buapoliv@gmail.com>
2024-09-12 18:38:24 +0300

    fix 2 marks

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class PageLimitPagination(PageNumberPagination):
     page_size_query_param = 'limit'
-    max_page_size = 4
+    max_page_size = 6
     page_size = 6
--- backend/users/validators.py
+++ backend/users/validators.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ValidationError
 def validate_alfanumeric_content(data):
     if not match(r'^[a-zA-Zа-яА-ЯёЁ\s\-.()]*$', data):
         raise ValidationError(
-            'Недопустимые символы в имени рецепта. ($%^&#:;!).'
+            'Недопустимые символы в имени пользователя. ($%^&#:;!).'
         )
 
 
